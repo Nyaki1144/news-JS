d4a1676eb8d0f917402b324b519b084e2d340283
Nyaki1144 <narek1144@gmail.com>
2022-07-04 13:47:03 +0400

    fix: fix config for TS

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '1e9785b25ebb49c4be26ca0ae54480c1', // получите свой ключ https://newsapi.org/
         });
     }
 }
