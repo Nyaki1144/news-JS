20d38baa4858cf62f770ea2cb9d76eb2150cab83
Nyaki1144 <narek1144@gmail.com>
2022-07-04 14:24:48 +0400

    feat: change loader.js to loader.ts

--- src/components/controller/loader.ts
+++ src/components/controller/loader.ts
@@ -1,19 +1,32 @@
+import { IData } from '../../components/types/types';
+
+type OptionsPar = {
+    [k: string]: string;
+};
+
+interface IGetResp {
+    endpoint: string;
+    options?: OptionsPar;
+}
+
 class Loader {
-    constructor(baseLink, options) {
-        this.baseLink = baseLink;
-        this.options = options;
+    constructor(readonly baseLink: string, readonly options: OptionsPar) {
+        // this.baseLink = baseLink;
+        // this.options = options;
     }
 
     getResp(
-        { endpoint, options = {} },
-        callback = () => {
+        { endpoint, options = {} }: IGetResp,
+        callback = (): void => {
             console.error('No callback for GET response');
         }
-    ) {
-        this.load('GET', endpoint, callback, options);
+    ): void {
+        if (options) {
+            this.load('GET', endpoint, callback, options);
+        }
     }
 
-    errorHandler(res) {
+    errorHandler(res: Response): Response {
         if (!res.ok) {
             if (res.status === 401 || res.status === 404)
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
@@ -23,7 +36,7 @@ class Loader {
         return res;
     }
 
-    makeUrl(options, endpoint) {
+    makeUrl(options: OptionsPar, endpoint: string) {
         const urlOptions = { ...this.options, ...options };
         let url = `${this.baseLink}${endpoint}?`;
 
@@ -34,7 +47,7 @@ class Loader {
         return url.slice(0, -1);
     }
 
-    load(method, endpoint, callback, options = {}) {
+    load(method: 'GET' | 'POST', endpoint: string, callback: (data: IData) => void, options: OptionsPar) {
         fetch(this.makeUrl(options, endpoint), { method })
             .then(this.errorHandler)
             .then((res) => res.json())
