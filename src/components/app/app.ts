0041f14555ca98850bbe13a9bdacb854fcdae4cf
Nyaki1144 <narek1144@gmail.com>
2022-07-11 11:03:32 +0400

    feat: delete comment in app.ts

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,6 +1,5 @@
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
-// import { SourcesResponse, ResponseArticle } from '../types/types';
 
 class App {
     controller: AppController;
